48dc72ca18caeffcefaf8cda5f66e9eddccd9918
lefasom <lefasom>
2023-01-04 21:02:47 -0300

    borro meta

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Home from './Home.jsx'
 export default function Index() {
   return (<>
   <Head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Chat</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp" />
